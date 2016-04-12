449ed7a032df8136054f2c74527a70bcd2d15e16
Aaron Greenspan <afg419@gmail.com>
2016-04-12 11:05:51 -0600

    Wires router to new counter controller

--- count_server/web/router.ex
+++ count_server/web/router.ex
@@ -16,7 +16,7 @@ defmodule CountServer.Router do
   scope "/", CountServer do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", CounterController, :index
   end
 
   # Other scopes may use custom stacks.
